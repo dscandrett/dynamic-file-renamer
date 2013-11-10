6c31bb2eef50707297b4e498910fc7dea0bee141
Doug Scandrett <doug@dssdigital.com>
2013-11-10 18:05:41 -0500

    Add in a regex option.
    Regex and Str replace are mutually exclusive.
    
    Include a bit more documentation as well.

--- README.md
+++ README.md
@@ -3,26 +3,47 @@ Dynamic File Renamer
 
 A small python script designed to help bulk renaming of files. There are a lot of other ways of doing this, obviously, but this one has some nice options, a clear command-line interface, and is nicely customizable.
 
-
 ### How to use
 ```
 usage: This script is designed to iterate over a given directory and change names of the files to specification
-       [-h] [-d FILE_DIR] -p STR_PATTERN [-r REPLACE_WITH] [-v]
+       [-h] (-n NEEDLE_STR | -r REGEX_STR) [-d FILE_DIR] [-s SUBSTITUTION_STR]
+       [-v]
 
 optional arguments:
   -h, --help            show this help message and exit
+  -n NEEDLE_STR, --needle NEEDLE_STR
+                        The string to match. This does not use regular
+                        expressions. To change a file extension, for example,
+                        you might pass |-n .jpg|
+  -r REGEX_STR, --regex REGEX_STR
+                        The regex string to match. This is a regular
+                        expression, and cannot be used with the -n flag
   -d FILE_DIR, --directory FILE_DIR
                         The directory to look through. Default is the current
                         working directory.
-  -p STR_PATTERN, --pattern STR_PATTERN
-                        The string to match. This does not use regular
-                        expressions. To find a file extension, for example,
-                        you might pass |-p .jpg|
-  -r REPLACE_WITH, --replacement REPLACE_WITH
-                        The replacement string. Default is to remove the
-                        matched string |empty string|)
+  -s SUBSTITUTION_STR, --substitution SUBSTITUTION_STR
+                        The substitution string. Default is to remove the
+                        matched string |empty string|. If the regex option is
+                        used, this will be a regex replacement. Note the empty
+                        default is mostly useless if the regex matches the
+                        entire string.
   -v, --verbose
 ```
+
+Here is an example using the regex option and the verbose flag (-v):
+```
+python dynamic_renamer.py -d ~/Desktop/test_dir/ -r '(.*?).doc' -v -s 'document.\1'
+Examining: |/Users/doug/Desktop/test_dir/| for regex pattern |(.*?).doc|
+|file1.txt| does not match pattern
+|file2.txt| does not match pattern
+|file3.txt| does not match pattern
+|file4.txt| does not match pattern
+---
+Match found |nonsense.doc|
+Now named |document.nonsense|
+---
+```
+
 ### License
 
 The MIT License (MIT)
--- dynamic_renamer.py
+++ dynamic_renamer.py
@@ -3,44 +3,59 @@
 """This script is designed to iterate over a given directory
     and change names of the files to specification"""
 
-import os, argparse, threading
-from os import rename
+import os, argparse, threading, re
 
 class DynamicFileRenamer(threading.Thread):
     
-    def __init__(self, file_dir, str_pattern, replace_with, verbose):
+    def __init__(self, file_dir, needle_str, regex_str, substitution_str, verbose):
         threading.Thread.__init__(self)
         self.file_dir = os.path.expanduser(file_dir)
-        self.str_pattern = str_pattern
-        self.replace_with = replace_with
+        self.needle_str = needle_str
+        self.regex_str = regex_str
+        self.substitution_str = substitution_str
         self.verbose = verbose
         
     def run(self):
+        if self.needle_str is not None:
+            self.log_if_needed('Examining: |{0}| for match |{1}|'.format(self.file_dir, self.needle_str))
+            for file_name in os.listdir(self.file_dir):
+                if self.needle_str in file_name:
+                    self.log_if_needed('---')
+                    self.log_if_needed('Found match with |{0}|'.format(file_name))
+                    new_file = file_name.replace(self.needle_str, self.substitution_str)
+                    os.rename(os.path.join(self.file_dir, file_name), os.path.join(self.file_dir, new_file))
+                    self.log_if_needed('Now named |{0}|'.format(new_file))
+                    self.log_if_needed('---')
+                else:
+                    self.log_if_needed('|{0}| does not match pattern'.format(file_name))
+        else:
+            self.log_if_needed('Examining: |{0}| for regex pattern |{1}|'.format(self.file_dir, self.regex_str))
+            for file_name in os.listdir(self.file_dir):
+                if re.match(r'{0}'.format(self.regex_str),file_name):
+                    self.log_if_needed('---')
+                    self.log_if_needed('Match found |{0}|'.format(file_name))
+                    new_file = re.sub(r'{0}'.format(self.regex_str), r'{0}'.format(self.substitution_str), file_name)
+                    os.rename(os.path.join(self.file_dir, file_name), os.path.join(self.file_dir, new_file))
+                    self.log_if_needed('Now named |{0}|'.format(new_file))
+                    self.log_if_needed('---')
+                else:
+                    self.log_if_needed('|{0}| does not match pattern'.format(file_name))
+
+    def log_if_needed(self, log_str):
         if self.verbose:
-            print 'Examining: |{0}| for match |{1}|'.format(self.file_dir, self.str_pattern)
-        for file_name in os.listdir(self.file_dir):
-            if self.str_pattern in file_name:
-                if self.verbose:
-                    print '---'
-                    print 'Found match with |{0}|'.format(file_name)
-                newFile = file_name.replace(self.str_pattern, self.replace_with)
-                rename(os.path.join(self.file_dir, file_name), os.path.join(self.file_dir, newFile))
-                if self.verbose:
-                    print 'Now named |{0}|'.format(newFile)
-                    print '---'
-            else:
-                if self.verbose:
-                    print '|{0}| does not match pattern'.format(file_name)
-        
+            print log_str
+
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("This script is designed to iterate over a given directory and change names of the files to specification")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-n', '--needle', dest='needle_str', default=None, help='The string to match. This does not use regular expressions. To change a file extension, for example, you might pass |-n .jpg|')
+    group.add_argument('-r', '--regex', dest='regex_str', default=None, help='The regex string to match. This is a regular expression, and cannot be used with the -n flag')
     parser.add_argument('-d', '--directory', dest='file_dir', default=os.getcwd(), help='The directory to look through. Default is the current working directory.')
-    parser.add_argument('-p', '--pattern', dest='str_pattern', required=True, help='The string to match. This does not use regular expressions. To find a file extension, for example, you might pass |-p .jpg|')
-    parser.add_argument('-r', '--replacement', dest='replace_with', default='', help='The replacement string. Default is to remove the matched string |empty string|)')
+    parser.add_argument('-s', '--substitution', dest='substitution_str', default='', help='The substitution string. Default is to remove the matched string |empty string|. If the regex option is used, this will be a regex replacement. Note the empty default is mostly useless if the regex matches the entire string.')
     parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true')
     args = parser.parse_args()
-    renamer = DynamicFileRenamer(args.file_dir,args.str_pattern,args.replace_with,args.verbose)
+    renamer = DynamicFileRenamer(args.file_dir, args.needle_str, args.regex_str, args.substitution_str, args.verbose)
     threadLock = threading.Lock()
     renamer.start()
     renamer.join()
